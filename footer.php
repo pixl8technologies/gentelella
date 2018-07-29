ceb763adefb72241c7483f476fe936a559ab0b42
Reynell Bobihis <reynellbobihis@gmail.com>
2018-07-29 13:00:58 +0800

    Created front page and add page

---
+++ footer.php
@@ -0,0 +1,24 @@
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="/student-hrms/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="/student-hrms/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="/student-hrms/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="/student-hrms/vendors/nprogress/nprogress.js"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script src="/student-hrms/build/js/custom.min.js"></script>
+  </body>
+</html>
